41ee209b94bd0fc50015fcf20953f7e3a406c75a
hagagmohamed702-create <hagagmohamed702@gmail.com>
2025-08-26 22:01:55 +0300

    Update manage.py

--- accounting_app/manage.py
+++ accounting_app/manage.py
@@ -1,22 +1,11 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
 import sys
+import os
 
+path = '/home/USERNAME/accounting_app'
+if path not in sys.path:
+    sys.path.append(path)
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings'
 
-
-if __name__ == '__main__':
-    main()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
